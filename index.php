ec71008f46704c14d615f4401d9a8fd442d13185
PoseidoIIIka <bashirov2005@yandex.ru>
2024-10-15 11:28:05 +0300

    Практика 4

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+class Cookie {
+    private $type;
+
+    private $availableTypes = ['шоколадное', 'овсяное', 'пряники', 'сахарное'];
+
+    public function setType($type) {
+        if (in_array($type, $this->availableTypes)) {
+            $this->type = $type;
+        } else {
+            echo "Тип печенья '$type' недоступен.\n";
+        }
+    }
+
+    public function getType() {
+        return $this->type ? $this->type : "Тип печенья не установлен.";
+    }
+}
+
+$cookie = new Cookie();
+
+$cookie->setType('шоколадное');
+echo "Тип печенья: " . $cookie->getType() . "\n"  . "<br>";
+
+$cookie->setType('пряники');
+echo "Тип печенья: " . $cookie->getType() . "\n"  . "<br>";
+
+$cookie->setType('ванильное');
